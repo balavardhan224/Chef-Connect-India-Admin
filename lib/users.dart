25bb40f2f3cf28fb0610beb9bea9652b936b6684
Balavardhan Reddy Malyala <balavardhanrdy@gmail.com>
2022-05-05 13:27:25 +0530

    user details

--- lib/users.dart
+++ lib/users.dart
@@ -9,7 +9,7 @@ class users extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Firestore pagination example'),
+        title: const Text('USERS'),
         centerTitle: true,
       ),
       body: PaginateFirestore(
@@ -18,20 +18,52 @@ class users extends StatelessWidget {
             PaginateBuilderType.listView, //Change types accordingly
         itemBuilder: (context, documentSnapshots, index) {
           final data = documentSnapshots[index].data() as Map?;
-          return ListTile(
-            leading: const CircleAvatar(child: Icon(Icons.person)),
-            title: data == null
-                ? const Text('Error in data')
-                : Text(data['firstname']),
+          return Container(
+            child: GestureDetector(
+              child: userlist(
+                firstname: data!['firstname'],
+                lastname: data['lastname'],
+                mobile1: data['mobile1'],
+                email: data['email'],
+                selectedLocation: data['selectedLocation'],
+                hiremode: data['hiremode'],
+              ),
+            ),
           );
         },
         // orderBy is compulsory to enable pagination
-        query:
-            FirebaseFirestore.instance.collection('users').orderBy('username'),
+        query: FirebaseFirestore.instance.collection('users'),
         itemsPerPage: 5,
         // to fetch real-time data
         isLive: true,
       ),
     );
+  }
+}
+
+class userlist extends StatefulWidget {
+  late String firstname;
+  late String lastname;
+  late String mobile1;
+  late String email;
+  late String selectedLocation;
+  late String hiremode;
+  userlist({
+    required this.firstname,
+    required this.lastname,
+    required this.mobile1,
+    required this.email,
+    required this.selectedLocation,
+    required this.hiremode,
+  });
+
+  @override
+  State<userlist> createState() => _userlistState();
+}
+
+class _userlistState extends State<userlist> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
